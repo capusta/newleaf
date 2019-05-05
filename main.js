76d3052b955e104044d9e8209a8af7a9bd8eb244
capusta <capusta@users.noreply.github.com>
2019-05-05 08:06:44 +0000

    working on routes

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 const Express = require('express')
 
 var app = Express();
-
-const bitcoin = require('bitcoinjs-lib')
-const bip39 = require('bip39')
-const bip32 = require('bip32')
-
-// using random words
-const mnemonic = process.env.MNEMONIC
-console.log(`using mnemonic: ${mnemonic}`)
-// create a seed
-const node = bip32.fromSeed(bip39.mnemonicToSeedSync(mnemonic))
-// segwit native electrum compatible
-const root_path = "m/84'/0'/0'"
-
-// generate the first 10 addresses in electrum wallet
-for (var i = 0; i < 5; ++i) {
-    const derive_path = root_path+"/0/"+i;
-    console.log(bitcoin.payments.p2wpkh({ pubkey: node.derivePath(derive_path).publicKey }).address)
-}
+var btcnode = require('./models/btcnode')
 
 app.set('trust proxy', true)
 
+app.use(function(req,res,next){
+    next();
+    })
+
+
 app.get("/",(req,res)=>{
     res.json('Hello World')
+})
+
+app.get('/sha',(req,res)=>{
 })
 
 const port = process.env.PORT;
